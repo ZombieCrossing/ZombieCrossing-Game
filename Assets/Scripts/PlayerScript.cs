57acb20992437cc2c83fdf51158f5dea2cac8863
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-01-06 18:38:44 +0100

    walking and sprinting mechanic added

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private const float JUMP_FORCE = 5;
+    private const float PLAYER_MOVEMENT_SPEED = 2;
 
     private bool spacePressed;
     private bool isGrounded;
+    private bool shiftPressed;
+    private float horizontalInput;
+    private float verticalInput;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -25,6 +29,18 @@ public class PlayerScript : MonoBehaviour
         {
             spacePressed=true;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            shiftPressed = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            shiftPressed=false;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     private void OnCollisionStay(Collision collision)
@@ -40,6 +56,17 @@ public class PlayerScript : MonoBehaviour
             isGrounded=false;
             spacePressed = false;
         }
+
+
+        float sprintSpeed = 1f;
+
+        if (shiftPressed)
+        {
+            Debug.Log("isSprinting");
+            sprintSpeed = 2;
+        }
+
+        rb.velocity = new Vector3(horizontalInput, rb.velocity.y / PLAYER_MOVEMENT_SPEED, verticalInput) * PLAYER_MOVEMENT_SPEED * sprintSpeed;
     }
 
 }
